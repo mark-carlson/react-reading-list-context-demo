3edf1f0a8a71dafc0ce0c43fc95d6658a67a99b2
Mark Carlson <mark.carlson@takeoff.com>
2020-03-10 22:15:40 -0700

    Converts Books page to hooks

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
@@ -6,113 +6,114 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
+import { BookContext } from '../context/BookContext';
 
-class Books extends Component {
-  state = {
-    books: [],
-    title: "",
+export const Books = () => {
+  const emptyForm = {
     author: "",
-    synopsis: ""
+    synopsis: "",
+    title: ""
   };
-
-  componentDidMount() {
-    this.loadBooks();
-  }
-
-  loadBooks = () => {
+  const [bookForm, setBookForm] = useState(emptyForm);
+  const { books, setBooks } = useContext(BookContext);
+    
+  const loadBooks = () => {
     API.getBooks()
-      .then(res =>
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
-  };
-
-  deleteBook = id => {
-    API.deleteBook(id)
-      .then(res => this.loadBooks())
-      .catch(err => console.log(err));
-  };
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.title && this.state.author) {
-      API.saveBook({
-        title: this.state.title,
-        author: this.state.author,
-        synopsis: this.state.synopsis
+      .then(res => {
+        setBooks(res.data);
+        setBookForm(emptyForm);
       })
-        .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  const deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setBookForm({
+      ...bookForm,
+      [name]: value
+    })
+  };
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    const { author, synopsis, title } = bookForm; 
+    if (title && author) {
+      API.saveBook({
+        title,
+        author,
+        synopsis,
+      })
+        .then(res => loadBooks())
         .catch(err => console.log(err));
     }
   };
 
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col size="md-6">
-            <Jumbotron>
-              <h1>What Books Should I Read?</h1>
-            </Jumbotron>
-            <form>
-              <Input
-                value={this.state.title}
-                onChange={this.handleInputChange}
-                name="title"
-                placeholder="Title (required)"
-              />
-              <Input
-                value={this.state.author}
-                onChange={this.handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                value={this.state.synopsis}
-                onChange={this.handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
-              />
-              <FormBtn
-                disabled={!(this.state.author && this.state.title)}
-                onClick={this.handleFormSubmit}
-              >
-                Submit Book
-              </FormBtn>
-            </form>
-          </Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-6">
+          <Jumbotron>
+            <h1>What Books Should I Read?</h1>
+          </Jumbotron>
+          <form>
+            <Input
+              value={bookForm.title}
+              onChange={handleInputChange}
+              name="title"
+              placeholder="Title (required)"
+            />
+            <Input
+              value={bookForm.author}
+              onChange={handleInputChange}
+              name="author"
+              placeholder="Author (required)"
+            />
+            <TextArea
+              value={bookForm.synopsis}
+              onChange={handleInputChange}
+              name="synopsis"
+              placeholder="Synopsis (Optional)"
+            />
+            <FormBtn
+              disabled={!(bookForm.author && bookForm.title)}
+              onClick={handleFormSubmit}
+            >
+              Submit Book
+            </FormBtn>
+          </form>
+        </Col>
+        <Col size="md-6 sm-12">
+          <Jumbotron>
+            <h1>Books On My List</h1>
+          </Jumbotron>
+          {books.length ? (
+            <List>
+              {books.map(book => (
+                <ListItem key={book._id}>
+                  <Link to={"/books/" + book._id}>
+                    <strong>
+                      {book.title} by {book.author}
+                    </strong>
+                  </Link>
+                  <DeleteBtn onClick={() => deleteBook(book._id)} />
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <h3>No Results to Display</h3>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
-export default Books;
